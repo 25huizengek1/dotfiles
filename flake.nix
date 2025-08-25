a5e9c49aee597370b22b1e3b305862c68662cc74
25huizengek1 <25huizengek1@gmail.com>
2025-08-25 11:02:11 +0200

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -73,7 +73,7 @@
           config.allowUnfree = true;
           config.android_sdk.accept_license = true;
           config.permittedInsecurePackages = [
-            "broadcom-sta-6.30.223.271-57-6.12.41"
+            "broadcom-sta-6.30.223.271-57-6.12.42"
           ];
           overlays = [
             self.overlays.default
