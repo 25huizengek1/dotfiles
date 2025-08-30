6078da98da5bf7cf40a4fbad41c71138cfe9afd8
25huizengek1 <25huizengek1@gmail.com>
2025-08-31 01:24:23 +0200

    add bart-laptop-new ssh pubkey

--- modules/users/bart.nix
+++ modules/users/bart.nix
@@ -31,6 +31,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKO4+0nbySi9L5GSXTExGCWdkZBqi5WEqYB9fr4LwKyh bart@bart-laptop"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMdc+Tbt0d+pHMYrDjrT3Ui09NV38T3bFWk/OMEL4Dp6 u0_a374@bart-phone"
       "sk-ecdsa-sha2-nistp256@openssh.com AAAAInNrLWVjZHNhLXNoYTItbmlzdHAyNTZAb3BlbnNzaC5jb20AAAAIbmlzdHAyNTYAAABBBG012Fvtbxykbn9hjOKRTe1ZK0ZksMK1j/ZnVrYqzuADZDuYCdGH5TB5znV+NbJuBmRuAWerBLr/rMTpY4frST4AAAAEc3NoOg== barto@bart-laptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE4zwjOqILG37umIJNYYSMjveYzmwjOw/pTdfLbcsaSP bart@bart-laptop-new"
     ];
   };
 
