8b9559111961168a3f0beb1c4febd838015419d3
25huizengek1 <25huizengek1@gmail.com>
2025-10-12 22:07:10 +0200

    update plasma config

--- home/plasma.nix
+++ home/plasma.nix
@@ -20,21 +20,28 @@
     shortcuts = {
       "services/Alacritty.desktop"."New" = "Meta+Return";
     };
+
     configFile = {
+      "kdeglobals"."General"."AccentColor" = "0,85,255";
       "kdeglobals"."General"."TerminalApplication" = "alacritty";
       "kdeglobals"."General"."TerminalService" = "Alacritty.desktop";
-      "kiorc"."Confirmations"."ConfirmDelete" = true;
+      "kiorc"."Confirmations"."ConfirmDelete" = false;
       "kiorc"."Confirmations"."ConfirmEmptyTrash" = true;
       "ksmserverrc"."General"."loginMode" = "emptySession";
-      "kwalletrc"."Wallet"."First Use" = false;
       "kwinrc"."Desktops"."Id_1" = "32f7aab7-d3d2-463f-897b-3795a776364c";
-      "kwinrc"."Desktops"."Number" = 1;
-      "kwinrc"."Desktops"."Rows" = 1;
+      "kwinrc"."Desktops"."Number" = 4;
+      "kwinrc"."Desktops"."Rows" = 2;
+      "kwinrc"."Effect-slide"."HorizontalGap" = 0;
+      "kwinrc"."Effect-slide"."VerticalGap" = 0;
+      "kwinrc"."ElectricBorders"."Bottom" = "ApplicationLauncher";
+      "kwinrc"."ElectricBorders"."Top" = "KRunner";
       "kwinrc"."Plugins"."blurEnabled" = true;
+      "kwinrc"."Plugins"."mousemarkEnabled" = true;
       "kwinrc"."Plugins"."sheetEnabled" = true;
       "kwinrc"."Plugins"."wobblywindowsEnabled" = true;
       "kwinrc"."Tiling"."padding" = 4;
-      "kwinrc"."Xwayland"."Scale" = 1;
+      "kwinrc"."Windows"."RollOverDesktops" = true;
+      "kwinrc"."Xwayland"."Scale" = 1.5;
       "kxkbrc"."Layout"."DisplayNames" = ",";
       "kxkbrc"."Layout"."LayoutList" = "us,us";
       "kxkbrc"."Layout"."Use" = true;
