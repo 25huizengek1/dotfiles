49e4d2d79d5611b829532e7c2dc985c4b92c32dd
Bart Oostveen <bart.oos.2006@gmail.com>
2025-11-14 13:13:57 +0100

    chore: nix flake update

--- modules/android.nix
+++ modules/android.nix
@@ -3,15 +3,10 @@
 let
   android = pkgs.androidenv.composeAndroidPackages {
     includeNDK = true;
-    buildToolsVersions = [
-      "35.0.0"
-      "35.0.1"
-      "36.0.0"
-    ];
-    platformVersions = [
-      "35"
-      "36"
-    ];
+    buildToolsVersions = [ "36.0.0" ];
+    platformVersions = [ "36" ];
+    includeSystemImages = true;
+    systemImageTypes = [ "google_apis_playstore" ];
     includeEmulator = true;
   };
 in
